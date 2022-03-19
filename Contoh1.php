36a205b7eff58bb0aaf0f3878a3793046f3a9769
rifkifdh <kesehatan2002@gmail.com>
2022-03-20 00:15:34 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+     public function index()
+     {
+         echo "<h1>Perkenalkan</h1>";
+         echo"Nama saya Rifki Fadhira
+              Saya tinggal di Tangerang
+              Olahraga yang saya sukai adalah
+              Futsal";
