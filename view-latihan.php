3d8e4a3933b3192623f2f09a89053664a3b3572e
rifkifdh <kesehatan2002@gmail.com>
2022-03-22 22:41:44 +0700

    menambahkan Latihan.php,Model_latihan1.php,view-latihan.php

---
+++ view-latihan.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita Belajar Web Programming. . !!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjualan yaitu <?=
+$nilai1 . "+" . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
