2bf8b0a85296c5c02c6da2cc9ab68ecccf79822a
rifkifdh <kesehatan2002@gmail.com>
2022-03-31 13:52:44 +0700

    menambahkan Web.php, v_footer.php, v_header.php, v_index.php

---
+++ Web.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit ('no direct script access allowed');
+
+class Web extends CI_Controller
+{
+
+    function _construct()
+    {
+        parent::_construct();
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        $data['judul'] = "Halaman Depan";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
+    }
+
+}
+     
